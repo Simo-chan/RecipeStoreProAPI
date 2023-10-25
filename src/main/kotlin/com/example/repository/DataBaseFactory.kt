861bda006f285b0de5ae898f9cb4e20f1078ebe9
Simo <semyon142000@gmail.com>
2023-10-25 22:00:50 +0300

    added to dbFactory

--- src/main/kotlin/com/example/repository/DataBaseFactory.kt
+++ src/main/kotlin/com/example/repository/DataBaseFactory.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.withContext
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.net.URI
 
 object DataBaseFactory {
 
@@ -25,10 +26,17 @@ object DataBaseFactory {
         val config = HikariConfig()
 
         config.driverClassName = System.getenv("JDBC_DRIVER")
-        config.jdbcUrl = System.getenv("DATABASE_URL")
         config.maximumPoolSize = 3
         config.isAutoCommit = false
         config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+
+        val uri = URI(System.getenv("DATABASE_URL"))
+        val userName = uri.userInfo.split(":").toTypedArray()[0]
+        val password = uri.userInfo.split(":").toTypedArray()[1]
+
+        config.jdbcUrl =
+            "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "sslmode=require" + "&user=$userName&password=$password"
+
         config.validate()
 
         return HikariDataSource(config)
